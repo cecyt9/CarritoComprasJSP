e3cad9c51f6910cdc46a408bc874ac850c1fffe0
emmanuel789 <eoropezag@ipn.mx>
2015-09-15 00:56:41 -0500

    Script de base de datos

---
+++ SimpleJspServletCrud/src/java/edu/cecyt9/util/Database.java
@@ -0,0 +1,26 @@
+package edu.cecyt9.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Database {
+      public static Connection getConnection() {
+          try  {
+              Class.forName("com.mysql.jdbc.Driver");
+              Connection con = DriverManager.getConnection
+                      ("jdbc:mysql://localhost:3306/cecyt9",
+                      "root","toor");
+              return con;
+          }
+          catch(Exception ex) {
+              System.out.println("Database.getConnection() Error -->" + ex.getMessage());
+              return null;
+          }
+      }
+
+       public static void close(Connection con) {
+          try  {
+              con.close();
+          }
+          catch(Exception ex) {
+          }
+      }
+}
